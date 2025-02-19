65a2e8da547d34fa57e0fe83082e8effde11e511
pjfmartins83 <pjfmartins83@gmail.com>
2025-02-19 17:31:13 -0300

    19/02/2025

---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ItemList from "./ItemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <ItemList
+        title="Artistas"
+        items={10}
+        itemsArray={artistArray}
+        path="/artists"
+        idPath="/artist"
+      />
+
+      <ItemList
+        title="Músicas"
+        items={20}
+        itemsArray={songsArray}
+        path="/songs"
+        idPath="/song"
+      />
+    </div>
+  );
+};
+
+export default Main;
